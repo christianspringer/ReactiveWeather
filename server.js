c6bafae6ceb3ddb512126d83cd87c753d22c7fea
christian <cfs2228@gmail.com>
2017-04-07 21:40:20 -0400

    prepped server once again

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-  if (req.headers(['x-forwarded-proto'] === 'http')) {
+app.use(function (req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
     next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url)
+  } else{
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
@@ -16,4 +16,4 @@ app.use(express.static('public'));
 
 app.listen(PORT, function () {
   console.log('Server is running on port: ' + PORT);
-})
+});
