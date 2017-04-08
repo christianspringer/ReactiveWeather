a799dd4136127bf7db6f1e2bf73fd5d5bcc94f8d
christian <cfs2228@gmail.com>
2017-04-08 10:06:59 -0400

    styled the nav bar

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -5,7 +5,6 @@ var Examples = (props) => {
         <div>
             <h3>Examples</h3>
             <p>Welcome to the examples page!</p>
-            <br/>
             <p>
                 This is just an example react component. Nothing special at the momemt,
                 but there will be more to come soon.
