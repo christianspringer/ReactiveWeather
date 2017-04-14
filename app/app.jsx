d9a8b8f486e85c83966337652b7716520b1d23dc
christian <cfs2228@gmail.com>
2017-04-14 09:34:42 -0400

    added sass

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ $(document).foundation();
 
 
 //App CSS
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
     <Router history={hashHistory}>
